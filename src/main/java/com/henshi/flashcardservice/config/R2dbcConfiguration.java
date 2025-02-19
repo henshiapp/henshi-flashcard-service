6a532b0dba6df8f6aca942c83f952a7ed61615c0
neumanf <fabricionewman@protonmail.com>
2025-02-19 20:18:27 -0300

    chore: init project

---
+++ src/main/java/com/henshi/flashcardservice/config/R2dbcConfiguration.java
@@ -0,0 +1,50 @@
+package com.henshi.flashcardservice.config;
+
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactoryOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+import org.springframework.lang.NonNull;
+
+import static io.r2dbc.spi.ConnectionFactoryOptions.*;
+
+@Configuration
+@Profile(value = "!test")
+public class R2dbcConfiguration extends AbstractR2dbcConfiguration {
+
+    @Value("${database.name}")
+    private String database;
+
+    @Value("${database.host}")
+    private String host;
+
+    @Value("${database.port:5432}")
+    private int port;
+
+    @Value("${database.username}")
+    private String username;
+
+    @Value("${database.password}")
+    private String password;
+
+    @Override
+    @Bean
+    @NonNull
+    public ConnectionFactory connectionFactory() {
+        return ConnectionFactories.get(
+            ConnectionFactoryOptions.builder()
+                .option(DRIVER, "postgresql")
+                .option(HOST, host)
+                .option(PORT, port)
+                .option(USER, username)
+                .option(PASSWORD, password)
+                .option(DATABASE, database)
+                .build()
+        );
+    }
+
+}
---
+++ src/test/java/com/henshi/flashcardservice/FlashcardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henshi.flashcardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
